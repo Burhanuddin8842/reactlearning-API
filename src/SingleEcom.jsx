18e5003011a96eae2fd8d039974400d3ae1da966
Burhanuddin <personalburhanuddin7@gmail.com>
2025-12-07 10:07:07 +0500

    done

--- src/SingleEcom.jsx
+++ src/SingleEcom.jsx
@@ -6,18 +6,23 @@ const SingleEcom = ({ thumbnail, title, description }) => {
     // let desc = 
     let [desc,setDesc]=useState(false)
     console.log(desc)
+    let [focus,setFocus] = useState(false)
     return (
         <>
-            <div className={`w-full p-1 ${desc ? 'opacity-65' : ''}`}>
-                <div className="shadow relative h-[60vh] shadow-md shadow-emerald-200 hover:shadow-lg hover:scale-105">
+            <div className={`w-full p-1`}>
+                <div className="shadow relative h-[60vh] shadow-md shadow-emerald-200 hover:shadow-lg hover:scale-105" onMouseEnter={()=>setFocus(!focus)} onMouseLeave={()=>setFocus(!focus)}>
                     <img src={thumbnail} alt="" className="" />
                     <div className="h-[60px]">
 
                         <h1 className=" my-3 font-sans text-xl text-bold uppercase text-gray-700 hover:text-black text-wrap overflow-clip">{title}</h1>
                     </div>
-                    <p className="text-gray-400 hover:text-gray-500 h-[30px] text-sm my-3 font-extralight" onMouseEnter={()=>setDesc(!desc)} onMouseLeave={()=>setDesc(!desc)}>{`${description.slice(0, 50)}...`}</p>
+                    <p className="text-gray-400 hover:text-gray-500 h-[30px] text-sm my-3 font-extralight cursor-pointer" onMouseEnter={()=>setDesc(!desc)} onMouseLeave={()=>setDesc(!desc)}>{
+                        focus ?
+                        `${description.slice(0,50)}`:
+                        `${description}`
+                        }</p>
 
-                <div className={`dialog-box absolute ${!desc ? 'hidden' : ''} left-full bottom-[30%]`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores fugiat voluptas nisi, beatae in repellat soluta sit laboriosam repudiandae quasi nostrum alias deleniti ab dicta odio. In, repudiandae! Nostrum, accusamus?</div>
+                {/* <div className={`dialog-box absolute ${!desc ? 'hidden' : ''} left-full bottom-[30%]`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores fugiat voluptas nisi, beatae in repellat soluta sit laboriosam repudiandae quasi nostrum alias deleniti ab dicta odio. In, repudiandae! Nostrum, accusamus?</div> */}
                 </div>
             </div>
 
